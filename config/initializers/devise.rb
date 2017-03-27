9199284b6fd810d5029e459277b23bcfb1b35635
michele de bruyn <debruyn.mm@gmail.com>
2017-03-27 15:48:39 +0200

    Create links on dashboard

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,8 +4,8 @@ Devise.setup do |config|
 
   config.omniauth :facebook, ENV["FB_ID"], ENV["FB_SECRET"],
   scope: 'email',
-  info_fields: 'email, first_name, last_name',
-  image_size: 'square',  # 50x50, guaranteed ratio
+  info_fields: 'email, first_name, last_name, id',
+  image_size: 'large',
   secure_image_url: true
 
 
