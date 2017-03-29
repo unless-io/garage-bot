8b06d593e6ce323310791f23921ca8c5a46f7ef4
michele de bruyn <debruyn.mm@gmail.com>
2017-03-29 15:27:54 +0200

    Add validations to questionaire

--- app/models/questionaire.rb
+++ app/models/questionaire.rb
@@ -2,4 +2,7 @@ class Questionaire < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :evaluations
   has_many :treatment_processes
+
+  validates :title, presence: true
+  validates :instruction, presence: true
 end
