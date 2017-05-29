b1f46de16cf23a30781d9470195d710644865c10
Lars Bohm <larsbohm@gmail.com>
2017-05-29 17:24:24 +0200

    temporary fix to disable bot

--- app/bot/conversation.rb
+++ app/bot/conversation.rb
@@ -2,15 +2,11 @@ include Facebook::Messenger
 include GarageBotHelper
 
 
-begin  
-  Bot.on :message do |message|
-    if bot_user_exists?(message.sender["id"])
-      BotConversationService.new(message: message).call
-    else
-      BotFirstVisitService.new(message: message).call
-    end 
-  end  
-rescue Exception => e  
-  puts e.message  
-  puts e.backtrace.inspect  
-end  
+Bot.on :message do |message|
+  # if bot_user_exists?(message.sender["id"])
+  #   BotConversationService.new(message: message).call
+  # else
+  #   BotFirstVisitService.new(message: message).call
+  # end 
+  puts message.text
+end
