dfeb0123e6f31386f2fc329b4fac261dfe63cbe1
Lars Bohm <larsbohm@gmail.com>
2017-05-29 18:04:07 +0200

    disable quick fix filter

--- app/bot/conversation.rb
+++ app/bot/conversation.rb
@@ -3,10 +3,9 @@ include GarageBotHelper
 
 
 Bot.on :message do |message|
-  puts message.text
-  # if bot_user_exists?(message.sender["id"])
-  #   BotConversationService.new(message: message).call
-  # else
-  #   BotFirstVisitService.new(message: message).call
-  # end 
+  if bot_user_exists?(message.sender["id"])
+    BotConversationService.new(message: message).call
+  else
+    BotFirstVisitService.new(message: message).call
+  end 
 end
