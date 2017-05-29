a2da8df2e7c8b6c3872d38981102c6c4db0a12b6
Lars Bohm <larsbohm@gmail.com>
2017-05-29 14:13:24 +0200

    try to rescue permission denied

--- app/services/bot_first_visit_service.rb
+++ app/services/bot_first_visit_service.rb
@@ -10,6 +10,10 @@ class BotFirstVisitService
   private
 
   def initial_synchronisation_message(sender_id)
-    @message.reply(text: "Welkom bij de Garagebot! Klik op deze link om je account te activeren! \n\n #{ENV["BASE_URL"]}facebook_account_link?fb_messenger_id=#{sender_id}")
+    if sender_id.nil?
+      puts "Sender not permitted"
+    else
+      @message.reply(text: "Welkom bij de Garagebot! Klik op deze link om je account te activeren! \n\n #{ENV["BASE_URL"]}facebook_account_link?fb_messenger_id=#{sender_id}")
+    end
   end
 end
